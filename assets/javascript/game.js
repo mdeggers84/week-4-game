b07790b07fb8a73865aee9a664d0c5a7b586b1ba
Michael Eggers <mdeggers84@gmail.com>
2017-03-13 17:39:32 -0400

    major design revision

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,8 @@ $(document).ready(function() {
 			char1: {
 				name: "Jon Snow",
 				value: "char1",
-				img: '<img src="assets/images/jon-snow.jpg" alt="Jon Snow">',
+				img: '<img src="assets/images/jon-snow.jpg" class="img-thumbnail" alt="Jon Snow">',
+				imgLose: '<img src="assets/images/sothdoll.gif" class="img-thumbnail" alt="sloth">',
 				hp: 100,
 				maxHP: 100,
 				ap: 10,
@@ -19,7 +20,8 @@ $(document).ready(function() {
 			char2: {
 				name: "Arya Stark",
 				value: "char2",
-				img: '<img src="assets/images/arya-stark.jpg" alt="Arya">',
+				img: '<img src="assets/images/arya-stark.jpg" class="img-thumbnail" alt="Arya">',
+				imgLose: '<img src="assets/images/sothdoll.gif" class="img-thumbnail" alt="sloth">',
 				hp: 80,
 				maxHP: 80,
 				ap: 20,
@@ -31,7 +33,8 @@ $(document).ready(function() {
 			char3: {
 				name: "Sandor Clegane",
 				value: "char3",
-				img: '<img src="assets/images/sandor-clegane.jpg" alt="Sandor Clegane">',
+				img: '<img src="assets/images/sandor-clegane.jpg" class="img-thumbnail" alt="Sandor Clegane">',
+				imgLose: '<img src="assets/images/sothdoll.gif" class="img-thumbnail" alt="sloth">',
 				hp: 120,
 				maxHP: 120,
 				ap: 30,
@@ -43,7 +46,8 @@ $(document).ready(function() {
 			char4: {
 				name: "White Walker",
 				value: "char4",
-				img: '<img src="assets/images/white-walker.jpg" alt="White Walker">',
+				img: '<img src="assets/images/white-walker.jpg" class="img-thumbnail" alt="White Walker">',
+				imgLose: '<img src="assets/images/sothdoll.gif" class="img-thumbnail" alt="sloth">',
 				hp: 5,
 				maxHP: 5,
 				ap: 0,
@@ -56,6 +60,9 @@ $(document).ready(function() {
 
 		var hero;
 		var defender;
+		var defeatedEnemies = 0;
+		var heroSelected = false;
+		var defenderSelected = false;
 		var gameReady = false;
 
 		function setBoard() {
@@ -64,59 +71,36 @@ $(document).ready(function() {
 			}
 		}
 
+		function resetBoard() {
+			defeatedEnemies = 0;
+			heroSelected = false;
+			defenderSelected = false;
+			gameReady = false;
+
+			$("#game-status").empty();
+			$("#hero-hp, #defender-hp").attr("style", "width: 100%;");
+
+			$(".char-select").show();
+			$("#replay, #status, .char-arena").hide();
+
+			setBoard();
+		}
+
+		function nextRound() {
+			defenderSelected = false;
+			gameReady = false;
+
+			$("#game-status").empty();
+			$("#defender, #next-round, #status").hide();
+			$("#directions").html("Choose your next opponent");
+
+		}
+
 		function setChar(sel, obj) {
+			obj.hp = obj.maxHP;
 			$(sel + " .char-name").html(obj.name);
 			$(sel + " .char-img").html(obj.img);
 			$(sel + " .char-status").html("health: " + obj.hp);
-		}
-
-		function chooseHero() {
-			var obj;
-
-			$("#heroes .char").on("click", function() {
-				$("#heroes .char").off("click");
-				obj = charObj[($(this).attr("value"))];
-				hero = obj;
-				obj.hero = true;
-
-				$("#char-1 .char-img").html(obj.img);
-				$("#char-1").addClass("hero");
-				$("#char-2, #char-3, #char-4").hide();
-
-				setEnemies();
-			});
-		}
-
-		function setEnemies() {
-			var j = 1;
-			var obj;
-
-			for (var i = 1; i <= 4; i++) {
-				obj = charObj["char" + i];
-
-				if (obj.hero === false) {
-					setChar("#enemy-" + j, obj);
-					$("#enemy-" +j).attr("value", "char" + i).show();
-					j++;
-				}
-
-			}
-		}
-
-		function chooseDefender() {
-			var obj;
-
-			$(".enemy").on("click", function() {
-				$(".enemy").off("click");
-				obj = charObj[($(this).attr("value"))];
-				defender = obj;
-				($(this)).hide();
-				setChar("#defender-1", obj);
-				$("#defender-1").show().addClass("defender");
-				$("#defender-status").show();
-				showHealth();
-				gameReady = true;
-			});
 		}
 
 		function showHealth() {
@@ -131,56 +115,111 @@ $(document).ready(function() {
 			return (a / b) * 100;
 		}
 
-		// fix this
 		function attack() {
 			var percent;
 
 			defender.hp = defender.hp - hero.ap;
 			percent = healthPercent(defender.hp, defender.maxHP);
 
-			$("#defender-1 .char-status").html(defender.hp);
+			$("#hero-attack").html("You hit " + defender.name + " for <strong>" + hero.ap + "</strong>.");
+
+			$("#defender .char-status").html("health: " + defender.hp);
 			$("#defender-hp").attr("style", "width: " + percent + "%;");
 
 			if (defender.hp > 0) {
 				hero.hp = hero.hp - defender.cap;
 				percent = healthPercent(hero.hp, hero.maxHP);
 
-				$("#char-1 .char-status").html(hero.hp);
+				$("#defender-attack").html(defender.name + " hit you for <strong>" + defender.cap + "</strong>.");
+
+				$("#hero .char-status").html("health: " + hero.hp);
 				$("#hero-hp").attr("style", "width: " + percent + "%;");
 
-				if (hero.hp > 0) {
-					hero.ap += hero.ap;
-				} else {
-					heroDeath();
-				}
-			} else {
-				defenderDeath();
 			}
 		}
 
 		function heroDeath() {
-			alert("you lose");
+			$("#attack").hide();
+			$("#replay").show();
+
+			$("#hero-attack, #defender-attack").empty();
+			$("#hero .char-img").html(hero.imgLose);
+			$("#game-status").html("You lose!! - Please try again!");
 		}
 
 		function defenderDeath() {
-			alert("you win");
+			defeatedEnemies ++;
+
+			if (defeatedEnemies === 3) {
+				alert("you win");
+				$("#hero .char-img").html(hero.imgLose);
+			} else {
+				$("#attack").hide();
+				$("#next-round").show();
+
+				$("#defender .char-img").html(defender.imgLose);
+				$("#hero-attack, #defender-attack").empty();
+				$("#game-status").html("You defeated " + defender.name + "! - click to start the next round.");
+			}
+			
 		}
 
 		// start function calls
 		setBoard();
-		chooseHero();
-		chooseDefender();
+		
+		$("#chars .char").on("click", function () {
+			if (heroSelected === false) {
+
+				heroSelected = true;
+				obj = charObj[($(this).attr("value"))];
+				hero = obj;
+				obj.hero = true;
+
+				$($(this)).hide();
+				$("#hero").show();
+				setChar("#hero", obj);
+
+				$("#directions").html("Choose your opponent");
+
+			} else if (heroSelected === true && defenderSelected === false) {
+
+				defenderSelected = true;
+				obj = charObj[($(this).attr("value"))];
+				defender = obj;
+
+				($(this)).hide();
+				$("#defender").show();
+				$("#attack").show();
+				$("#status").show();
+
+				$("#defender-hp").attr("style", "width: 100%;");
+				$("#directions").html("Fight!!");
+
+				setChar("#defender", obj);
+				showHealth();
+
+				gameReady = true;
+
+			}
+		});
 
 		$("#attack").on("click", function() {
 			if (gameReady) {
 				attack();
-				// updateStatus();
 				if (hero.hp <= 0) {
 					heroDeath();
 				} else if (defender.hp <= 0) {
 					defenderDeath();
 				}
 			}
+		});
+
+		$("#replay").on("click", function() {
+			resetBoard();
+		});
+
+		$("#next-round").on("click", function() {
+			nextRound();
 		});
 	}
 	// end game function
